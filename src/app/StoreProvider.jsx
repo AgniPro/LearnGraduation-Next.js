b4844c52c6ec79a7c01cd74a86bf6fda03f1d665
Abhishek Kumar Mehta <84276343+AgniPro@users.noreply.github.com>
2024-08-04 10:30:06 +0530

    Delete src directory

--- src/app/StoreProvider.jsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import { Provider } from 'react-redux'
-import { store } from '@/lib/store'
-
-export default function StoreProvider({ children }) {
-
-  return <Provider store={store}>{children}</Provider>
-}
