66c6c74f16522e5f85c99f10817a65385ba7f4c4
Abhishek Kumar Mehta <84276343+AgniPro@users.noreply.github.com>
2024-08-12 20:40:58 +0530

    Update TogleCheckbox.jsx

--- src/components/LayoutComponents/TogleCheckbox.jsx
+++ src/components/LayoutComponents/TogleCheckbox.jsx
@@ -3,12 +3,13 @@
 import { useState, useEffect } from 'react';
 
 const ToggleCheckbox = ({ htmlFor, children }) => {
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 896);
+    const [isMobile, setIsMobile] = useState(false);
 
     useEffect(() => {
         const handleResize = () => {
             setIsMobile(window.innerWidth < 896);
         };
+        handleResize();
 
         window.addEventListener('resize', handleResize);
         return () => {
