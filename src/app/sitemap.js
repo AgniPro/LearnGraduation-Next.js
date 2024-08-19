d6d50574ca0a08b8196e86c5b1d0081e9c5d939f
Abhishek Kumar Mehta <84276343+AgniPro@users.noreply.github.com>
2024-08-19 15:37:15 +0530

    Update sitemap.js

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,22 +1,18 @@
-
 export default async function sitemap() {
     const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/get-posts?skip=0&limit=1000`);
-    const posts  = await response.json();
+    const posts = await response.json();
 
     const postEntries = posts.map((post) => ({
         url: `${process.env.NEXT_PUBLIC_URL}/posts/${post.url}`,
-        lastModified: new Date(post.updatedAt),
-        sitemapSize: 7000,
+        lastModified: new Date(post.updatedAt).toISOString(),
         changefreq: 'daily',
         priority: 0.7,
-        exclude: ['/admin/*', '/login'],
-
     }));
 
     return [
         {
             url: `${process.env.NEXT_PUBLIC_URL}/about`,
-            lastModified: new Date(),
+            lastModified: new Date().toISOString(),
         },
         ...postEntries,
     ];
