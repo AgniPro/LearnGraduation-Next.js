040b35b04d3bfb79532805424a4554d1033c1638
Abhishek Kumar Mehta <84276343+AgniPro@users.noreply.github.com>
2025-03-29 14:49:52 +0530

    Add files via upload

---
+++ src/app/admin/manage-users/page.jsx
@@ -0,0 +1,14 @@
+import AllUsers from "@/components/Admin/AllUsers";
+import React from "react";
+
+const Page = () => {
+  return (
+     <div>
+      <h2 className="text-2xl font-semibold">Manage Users</h2>
+      <p>This Feature is comming soon...</p>
+      <AllUsers />
+      </div>
+  );
+};
+
+export default Page;
